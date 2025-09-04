c878e7b3d893d303bb3c5766bbe8c505cf211919
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2025-09-04 09:43:18 +0530

    commit

---
+++ C Programming/4-9/main (4).c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  for(int i=1;i<=10;i++)
+  {
+      
+      if(i==5)
+      {
+         // break;
+        continue;
+          
+      }
+      
+      printf("%d",i);//1//2//3//4//6
+      
+  }
+
+    return 0;
+}
